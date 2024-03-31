4419a1ffbaae9ec37cf3367cdc18348f9a87cf6d
alvarezfelipedev <n.felialvarez@gmail.com>
2024-03-31 17:11:03 -0300

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section>
+      <div>
+        <ul>
+          <a href="https://thiagonapoli.myportfolio.com/" target='_blank'>
+            <li>Portafolio</li>
+          </a>
+          <a href="" target='_blank'>
+            <li>Behance</li>
+          </a>
+          <a href="https://www.youtube.com/@thiagonapoli_" target='_blank'>
+            <li>Youtube</li>
+          </a>
+          <a href="https://www.instagram.com/joelnapoli_/" target='_blank'>
+            <li>Instagram</li>
+          </a>
+
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default App
